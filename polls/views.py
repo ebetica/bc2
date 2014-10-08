1925a2c697b96e7d0b44daa0da1db5a8e7bd7775
Mister Abc <zl4ry@virginia.edu>
2014-10-08 03:09:35 -0400

    Got ranked choice and multiple choice to work\!

--- polls/views.py
+++ polls/views.py
@@ -28,8 +28,45 @@ class ResultsView(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(ResultsView, self).get_context_data(**kwargs)
+        p = context['poll']
+        total = p.voter_set.count()
+        context['total'] = total
         choices = context['poll'].choice_set.all()
-        context['map'] = {choice.choice_text: len(choice.vote_set.all()) for choice in choices}
+        if p.type in [p.SIMPLE, p.MULTI]:
+            context['map'] = sorted([(choice, choice.vote_set.count()) for choice in choices],
+                                    key=lambda x: x[1],
+                                    reverse=True)
+        elif p.type == p.RANKED:
+            losers = []
+            winners = []
+            rest = [choice for choice in p.choice_set.all()]
+            test = []
+            while rest:
+                ctr = {r:[0 for i in rest] for r in rest}
+                for voter in p.voter_set.all():
+                    votes = [vote for vote in voter.vote_set.all()]
+                    votes.sort(key=lambda vote: vote.ranking)
+                    ind = 0
+                    for i in votes:
+                        c = i.choice
+                        if c in rest:
+                            ctr[c][ind] = ctr[c][ind] + 1
+                            ind+=1
+                ranking = sorted(ctr.items(), key=lambda x: x[1], reverse=True)
+                if ranking[0][1][0] > total/2:
+                    winners.append(ranking[0][0])
+                    rest.remove(ranking[0][0])
+                else:
+                    losers.append(ranking[-1][0])
+                    rest.remove(ranking[-1][0])
+                test.append(ctr)
+            ranking = winners + list(reversed(losers))
+            respective_votes = [", ".join(
+                [str(choice.vote_set.filter(ranking=k).count()) for k in range(len(ranking))])
+                                for i,choice in enumerate(ranking)
+                               ]
+            context['map'] = zip(ranking, respective_votes)
+
         return context
 
 def vote(request, poll_id):
@@ -61,8 +98,25 @@ def vote(request, poll_id):
             # user hits the Back button.
             return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
     elif p.type == p.RANKED:
+        try:
+            voter = Voter.objects.get(user=request.user, poll=p)
+        except Voter.DoesNotExist:
+            voter = Voter(poll=p, user=request.user)
+            voter.save()
+        for vote in voter.vote_set.all():
+            vote.delete()
+        rank = request.POST.get('rank').split(',')
+        for i,e in enumerate(rank):
+            choice = p.choice_set.get(pk=e)
+            vote = Vote(voter=voter)
+            vote.choice = choice
+            vote.ranking = i;
+            vote.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+    else:
         return render(request, 'polls/detail.html', {
             'poll': p,
-            'error_message': "Not implemented",
+            'error_message': "Poll not implemented :(",
             })
+
 
